917c1c42162c078195e9f4780fbe64869b038edd
Dagim <dagimgetaW27@gmail.com>
2026-04-22 11:47:32 +0300

    chore: explain statically typed language and it's data type subsets

---
+++ exercises/3_common_programming_concepts/3.2 data_types/introduction/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // rust is statically typed language - means the type of every variable is known at compile time
+    // there are two data type subsets: scalar and compound.
+
+    // one - scalar
+    // it represents a single value
+    // rust has four primary scalar types: integers, floating-point numbers, Booleans, and characters
+
+    // two - compounf
+    // can group multiple values into one type
+    // rust has two primitive compound types: tuples and arrays.
+}
