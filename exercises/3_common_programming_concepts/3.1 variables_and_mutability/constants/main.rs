8932bb6844fef1b6b50a0ffca908c45c6731c766
Dagim <dagimgetaW27@gmail.com>
2026-04-22 10:29:22 +0300

    chore: explain constant variable and basic rules

---
+++ exercises/3_common_programming_concepts/3.1 variables_and_mutability/constants/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("there are {} seconds in 3 hours", THREE_HOURS_IN_SECONDS);
+
+    // constant are not immutable as a default they are immutable always
+    // to create constant variable use all CAPITAL LETTER and _ for spaces
+}
