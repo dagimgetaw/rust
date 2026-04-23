25053eff9e30336ba8badbf2a2cbba6e162622e9
Dagim <dagimgetaW27@gmail.com>
2026-04-23 18:51:43 +0300

    feat: call another_function from the main function

---
+++ exercises/3_common_programming_concepts/3.3 functions/functions/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("main function");
+    another_function();
+
+    // when creating a function use small letter and underscore for spacing 
+}
+
+fn another_function() {
+    println!("another function");
+}
