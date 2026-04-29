5b29dea0da574c498d21cd688e640714fbad3b8a
Dagim <dagimgetaW27@gmail.com>
2026-04-29 15:09:21 +0300

    chore: summerize the concepts of ownership, borrowing, and slices

---
+++ exercises/4_understanding_ownership/4.3 the_slice_type/summary/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    // the concepts of ownership, borrowing, and slices ensure memory safety in rust programs at compile time. 
+    // the rust language gives you control over your memory usage in the same way as other systems programming languages. 
+    // but having the owner of data automatically clean up that data when the owner goes out of scope means you don’t have to write and debug extra code to get this control.
+
+} 
