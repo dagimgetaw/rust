380823ed01684990cf7d207cc85a86ace3e91fe3
Dagim <dagimgetaW27@gmail.com>
2026-04-29 14:48:07 +0300

    feat: explain how ownership moves and how to borrow pointer

---
+++ exercises/4_understanding_ownership/4.2 references_and_borrowing/references_and_borrowing/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let s1 = String::from("hello"); // s1 owns the data
+
+    let s2 = s1; // ownership moves to s2
+
+    // println!("{s1}"); error: s1 is no longer valid
+
+    println!("{s2}"); // ok
+
+
+    let r1 = String::from("hello"); // s1 owns data
+
+    let r2 = &r1; // r1 borrows s1 (no ownership change)
+
+    println!("{r1}"); // still valid
+    println!("{r2}"); // also valid
+}
