ef055c0364203408acbe6b689b0655ada559a9f8
Dagim <dagimgetaW27@gmail.com>
2026-04-30 13:22:37 +0300

    feat: create unit-like structs

---
+++ exercises/5_using_structs_to_structure_related_data/5.1 defining_and_instantiating_structs/defining_unit-like_structs/main.rs
@@ -0,0 +1,8 @@
+struct AlwaysEqual;
+
+fn main() {
+    let subject = AlwaysEqual;
+
+    // you can also define structs that don’t have any fields.
+    // these are called unit-like structs because they behave similarly to ()
+}
